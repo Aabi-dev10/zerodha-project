eb9e8d23d0076b0c704e2d4d760ab51135a6bfd7
Areeba Ashfaq <areebaashfaq990@gmail.com>
2026-09-20 10:14:25 -0700

    Fix import typo from ajax to axios in Home.jsx

--- zerodha-dashboard/src/components/Home.jsx
+++ zerodha-dashboard/src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import { useCookies } from "react-cookie";
-import axios from "ajax";
+import axios from "axios";
 
 import Dashboard from "./Dashboard.jsx"; 
 
